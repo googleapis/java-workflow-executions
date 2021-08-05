9ef4f4298510bbe481c83ce1af17da55463c6344
yoshi-automation <yoshi-automation@google.com>
2021-08-05 02:23:18 -0700

    chore: release gapic-generator-java v1.0.17
    Committer: @miraleung
    PiperOrigin-RevId: 388499329
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 3 11:12:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bb0a090d9204110042ab5dee2ce9e06e8071ce54
    Source-Link: https://github.com/googleapis/googleapis/commit/bb0a090d9204110042ab5dee2ce9e06e8071ce54

--- google-cloud-workflow-executions/src/main/java/com/google/cloud/workflows/executions/v1/stub/GrpcExecutionsStub.java
+++ google-cloud-workflow-executions/src/main/java/com/google/cloud/workflows/executions/v1/stub/GrpcExecutionsStub.java
@@ -243,7 +243,13 @@ public class GrpcExecutionsStub extends ExecutionsStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
